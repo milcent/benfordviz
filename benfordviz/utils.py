10f323a359b011802c22b0179e191011434a09d8
Marcel Milcent <marcelmilcent@gmail.com>
2021-05-22 22:49:47 -0300

    util funcs to be needed by every plotting lib

---
+++ benfordviz/utils.py
@@ -0,0 +1,18 @@
+from numpy import array, ceil, log10, sqrt, where
+
+def _get_upper_lower_bounds(expect_dist, sample_size:int, critical_z:float):
+    common_1 = critical_z * (sqrt(expect_dist * (1 - expect_dist) / sample_size))
+    common_2 = 1 / (2 * sample_size)
+    upper = expect_dist + common_1 + common_2
+    lower = expect_dist - common_1 - common_2
+    return upper, lower #where((lower > 0), lower, 0)
+
+def _get_in_out_bound_colors_(found, upper, lower, in_colors, out_colors):
+    out_bounds = (found > upper) | (found < lower)
+    colors = in_colors.copy()
+    colors[out_bounds] = out_colors
+    return colors
+
+def _set_n_int_places_(array_len:int): return int(ceil(log10(array_len)))
+
+def _get_x_range_(arr): return arr[0] - 1, arr[-1] + 1
