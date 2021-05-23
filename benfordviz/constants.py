bd155dea1a9ec5a207da4a69c6168f4469baee7a
Marcel Milcent <marcelmilcent@gmail.com>
2021-05-22 22:49:03 -0300

    colors and tooltips

--- benfordviz/constants.py
+++ benfordviz/constants.py
@@ -1,1 +1,19 @@
-from benford.constants import colors
+from .config import get_color_mode
+
+colors = get_color_mode()
+
+FOUND_BAR_COLOR = colors["m"]
+OUT_OF_BOUNDS_BAR_COLOR = colors["af"]
+EXPECT_LINE_COLOR = colors["s"]
+BACKGROUND_COLOR = colors["b"]
+EXPECT_BAR_COLOR = colors["t"]
+
+TOOLTIPS_BASE = [
+    # insert ("Digits", f"@{dft.index.name}" + "{" + n_int_places * "0" +
+    #  "}") in pos 0
+    ("Expected", "@Expected{0.0000%}"),
+    ("Found", "@Found{0.0000%}"),
+    ("Upper Bound", "@upper{0.0000%}"),
+    ("Lower Bound", "@lower{0.0000%}")
+]
+
