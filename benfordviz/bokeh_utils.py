5612362972f749c4eb9e9d6d49fec465cdb80f11
Marcel Milcent <marcelmilcent@gmail.com>
2021-05-23 16:50:29 -0300

    chart-related functions incorporated in add_figure

--- benfordviz/bokeh_utils.py
+++ benfordviz/bokeh_utils.py
@@ -11,16 +11,6 @@ from .utils import (_get_in_out_bound_colors_, _get_upper_lower_bounds,
                     _get_x_range_, _set_n_int_places_, _get_base_bar_colors_)
 
 
-def _add_base_figure_(digits_test, n_int_places):
-    max_y = max(digits_test.Found.max(), digits_test.Expected.max())
-    fig = figure(
-        sizing_mode="stretch_width", x_range=_get_x_range_(digits_test.index),
-        y_range=(0, max_y + 10 ** -(n_int_places + 1)),
-        title=digits_test.name, x_axis_label="Digits",
-        y_axis_label="Found vs. Expected Proportions (%)"
-    )
-    return fig
-
 def _get_tooltips_(name:str, n_int_places:int,
                    base_tooltips:list=TOOLTIPS_BASE,
                    upper_lower=True):
@@ -31,42 +21,52 @@ def _get_tooltips_(name:str, n_int_places:int,
         tooltips  = tooltips[:-2]
     return tooltips
 
-def _add_glyphs_(fig, data, confidence=None):
-    
-    source = ColumnDataSource(data)
-    name = data.index.name
+# LOOONG function! Could be broken into at least three, but did not see 
+# the need for separation
+def add_figure(digit_test):
+
+    max_y = max(digit_test.Found.max(), digit_test.Expected.max())
+    len_dig = len(digit_test)
+    n_int_places = _set_n_int_places_(len_dig)
+    ratio = 3 if len_dig == 100 else 1.5
+
+    fig = figure(
+        title=digit_test.name, x_range=_get_x_range_(digit_test.index),
+        y_range=(0, max_y + 10 ** -(n_int_places + 1)), aspect_ratio=ratio,
+        sizing_mode="scale_both", x_axis_label="Digits",
+        y_axis_label="Found vs. Expected Proportions (%)"
+    )
+    source = ColumnDataSource(digit_test)
+    name = digit_test.index.name
 
     fig.line(x=name, y="Expected", width=2, legend_label="Expected",
              source=source, color=EXPECT_LINE_COLOR, level="overlay")
 
-    source.data["colors"] = _get_base_bar_colors_(len(data))
+    source.data["colors"] = _get_base_bar_colors_(len_dig)
 
-    if not confidence:
+    if not digit_test.critical_values["Z"]:
         fig.vbar(x=name, top="Found", width=.75, color="colors",
                  source=source, legend_label="Found")
-        tooltips = _get_tooltips_(name, _set_n_int_places_(len(data)),
-                                  upper_lower=False)
+        tooltips = _get_tooltips_(name, n_int_places, upper_lower=False)
         fig.add_tools(HoverTool(tooltips=tooltips, mode="vline"))
-        return fig
-
-    source.data["upper"], source.data["lower"] = _get_upper_lower_bounds(
-        data.Expected.values, data.N, data.critical_values["Z"]
-    )
-    source.data["colors"] = _get_in_out_bound_colors_(
-        data.Found.values, source.data["upper"], source.data["lower"],
-        source.data["colors"], OUT_OF_BOUNDS_BAR_COLOR
-    )
-    fig.vbar(x=name, top="Found", width=.75, legend_label="Found",
-             color="colors", source=source)
-    fig.varea(x=name, y1="lower", y2="upper", fill_alpha=.4, level="underlay",
-              source=source, fill_color=EXPECT_LINE_COLOR,
-              legend_label="Confidence Bounds")
-    tooltips = _get_tooltips_(name, _set_n_int_places_(len(data)))
-    fig.add_tools(HoverTool(tooltips=tooltips, mode="vline"))
-    return fig
-
-def _add_figure_specs_(fig, x_ticks, n_int_places:int):
-    fig.xaxis.ticker = FixedTicker(ticks=x_ticks)
+    else:
+        source.data["upper"], source.data["lower"] = _get_upper_lower_bounds(
+            digit_test.Expected.values, digit_test.N,
+            digit_test.critical_values["Z"]
+        )
+        source.data["colors"] = _get_in_out_bound_colors_(
+            digit_test.Found.values, source.data["upper"], source.data["lower"],
+            source.data["colors"], OUT_OF_BOUNDS_BAR_COLOR
+        )
+        fig.vbar(x=name, top="Found", width=.75, legend_label="Found",
+                color="colors", source=source)
+        fig.varea(x=name, y1="lower", y2="upper", fill_alpha=.4, level="underlay",
+                source=source, fill_color=EXPECT_LINE_COLOR,
+                legend_label="Confidence Bounds")
+        tooltips = _get_tooltips_(name, n_int_places)
+        fig.add_tools(HoverTool(tooltips=tooltips, mode="vline"))
+ 
+    fig.xaxis.ticker = FixedTicker(ticks=digit_test.index.values)
     fig.xaxis.major_label_orientation = pi / 2
     fig.xgrid.visible, fig.ygrid.visible = False, False
     fig.xaxis[0].formatter = NumeralTickFormatter(format="0" * n_int_places)
@@ -75,4 +75,5 @@ def _add_figure_specs_(fig, x_ticks, n_int_places:int):
     fig.legend.background_fill_color = BACKGROUND_COLOR
     fig.legend.border_line_color = BACKGROUND_COLOR
     fig.legend.click_policy = "hide"
+    
     return fig
