dfe5a08b0a7b1edbe7ca461c7f1715f14aa58ff6
Marcel Milcent <marcelmilcent@gmail.com>
2021-06-14 07:27:16 -0300

    check mantissas data function

--- benfordviz/checks.py
+++ benfordviz/checks.py
@@ -1,4 +1,5 @@
-from benford import Test
+from numpy import array
+from benford import Test, Benford, Mantissas
 
 
 def _check_digit_test_(digit_test):
@@ -7,3 +8,15 @@ def _check_digit_test_(digit_test):
         " instances, which are attributes of the Benford class"
         " in the most recent benford_py API.")
     return digit_test
+
+
+def _check_mantissa_data_(data):
+    if isinstance(data, Benford):
+        data.mantisssas()
+        return data.Mantissas.data.Mantissa.values
+    elif isinstance(data, Mantissas):
+        return data.data.Mantissa.values
+    elif isinstance(data, array):
+        return data
+    raise TypeError("This operation accepts one of the following objects as "
+        "inputs: benford.Benford, benford.Mantissas, and np.array.")
